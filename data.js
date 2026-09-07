75165d4b6df27d97ca6d8d6b419a37604ca91a26
Ubiguy <100875824+Ubiguy@users.noreply.github.com>
2026-09-07 19:29:36 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -159,7 +159,9 @@ const MATCHES = [
   {date:'2026-09-02',aPlayer:'Imran',aSubFor:null,aPts:10,bPlayer:'Sid',bSubFor:null,bPts:15},
   {date:'2026-09-03',aPlayer:'Nav',aSubFor:null,aPts:13,bPlayer:'Jabar',bSubFor:null,bPts:16},
   {date:'2026-09-03',aPlayer:'Imran',aSubFor:null,aPts:13,bPlayer:'Tab',bSubFor:null,bPts:16},
-  {date:'2026-09-05',aPlayer:'Moody',aSubFor:null,aPts:13,bPlayer:'Raza',bSubFor:null,bPts:15}
+  {date:'2026-09-05',aPlayer:'Moody',aSubFor:null,aPts:13,bPlayer:'Raza',bSubFor:null,bPts:15},
+  {date:'2026-09-07',aPlayer:'Nav',aSubFor:null,aPts:14,bPlayer:'Sid',bSubFor:null,bPts:23},
+  {date:'2026-09-07',aPlayer:'Shahzad',aSubFor:null,aPts:9,bPlayer:'Tariq',bSubFor:null,bPts:10}
 ];
 
 /* The published 10-week round robin. A match may be played ahead of
